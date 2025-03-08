66a6e5b8b8da9ac1c12fec9017d805b8efbd9f43
yukinakanaka <yuki.nakamura@mapbox.com>
2025-03-08 10:50:14 +0900

    refactor: remove unused map and struct

--- tetragon-ebpf/src/maps.rs
+++ tetragon-ebpf/src/maps.rs
@@ -3,12 +3,8 @@ use aya_ebpf::{
     maps::{HashMap, LruHashMap, PerCpuArray, PerfEventArray, ProgramArray},
 };
 
-use crate::lib_data_msg::MsgData;
-use tetragon_common::process::{EventBytes, ExecveInfo, ExecveMapValue, KernelStats, ProcessEvent};
+use tetragon_common::process::{EventBytes, ExecveInfo, ExecveMapValue, KernelStats};
 use tetragon_common::vmlinux::{__u32, __u64};
-
-#[map(name = "PROCESS_EVENTS")]
-pub static PROCESS_EVENTS: PerfEventArray<ProcessEvent> = PerfEventArray::new(0);
 
 #[map(name = "EXECVE_MAP")]
 pub static EXECVE_MAP: HashMap<__u32, ExecveMapValue> = HashMap::with_max_entries(32768, 0);
@@ -29,9 +25,6 @@ pub static TCPMON_MAP: PerfEventArray<EventBytes> = PerfEventArray::new(0);
 // BPF_MAP_TYPE_PROG_ARRAY's primary use is as a jump table for the tail_call feature
 #[map(name = "EXECVE_CALLS")]
 pub static EXECVE_CALLS: ProgramArray = ProgramArray::with_max_entries(2, 0);
-
-#[map(name = "DATA_HEAP")]
-pub static mut DATA_HEAP: PerCpuArray<MsgData> = PerCpuArray::with_max_entries(1, 0);
 
 #[map(name = "EXECVE_MSG_HEAP_MAP")]
 pub static mut EXECVE_MSG_HEAP_MAP: PerCpuArray<EventBytes> = PerCpuArray::with_max_entries(1, 0);
